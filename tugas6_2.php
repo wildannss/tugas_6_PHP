8181605d08072c6e6306fbf3f11629508b5a6dff
wildan <wildan.syarif1021@gmail.com>
2019-11-05 17:15:12 +0700

    tugas niomic
    tugas niomic

---
+++ tugas6_2.php
@@ -0,0 +1,17 @@
+<?php
+
+    $j=15000;
+    $k=2000;
+    $jual=($j*6*5)+($k*2);
+
+    echo "===========================================================";
+    print ("<br />");
+    echo "Harga Jambu = Rp ".$j." / Kg";
+    print ("<br />");
+    echo "Harga Kardus = Rp ".$k." / Pcs";
+    print ("<br />");    
+    echo "Total Penjualan (Dus dan Jambu) = Rp ".$jual;
+    print ("<br />");
+    echo "===========================================================";
+
+?>
